f9d1550690fa82f23461be765613214246ca2c07
Eduard Venidiktov <edven7777777@gmail.com>
2024-08-19 17:48:15 +0300

    add localStorage

--- src/components/FavoritesContext/FavoritesContext.jsx
+++ src/components/FavoritesContext/FavoritesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const FavoritesContext = createContext();
 
@@ -8,10 +8,22 @@ export const useFavorites = () => {
 
 export const FavoritesProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
-  console.log(favorites);
+
+  useEffect(() => {
+    // Завантаження улюблених з localStorage при ініціалізації
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(storedFavorites);
+  }, []);
+
+  useEffect(() => {
+    // Зберігання улюблених в localStorage при зміні
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   const addFavorite = (camper) => {
-    setFavorites([...favorites, camper]);
+    if (!favorites.some((fav) => fav._id === camper._id)) {
+      setFavorites([...favorites, camper]);
+    }
   };
 
   const removeFavorite = (id) => {
