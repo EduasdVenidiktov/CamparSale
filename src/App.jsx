c713ca24f5d92e3ef6bc7b843daf71a01cc43705
Eduard Venidiktov <edven7777777@gmail.com>
2024-08-20 21:14:20 +0300

    add lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-// import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import HomePage from "./components/pages/HomePage/HomePage";
-import FavoritesPage from "./components/pages/FavoritesPage/FavoritesPage";
-import CatalogPage from "./components/pages/CatalogPage/CatalogPage";
 import Navigation from "./components/Navigation/Navigation";
 import css from "./App.module.css";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const HomePage = lazy(() => import("./components/pages/HomePage/HomePage"));
+const CatalogPage = lazy(() =>
+  import("./components/pages/CatalogPage/CatalogPage")
+);
+const FavoritesPage = lazy(() =>
+  import("./components/pages/FavoritesPage/FavoritesPage")
+);
 
 export default function App() {
   return (
     <div className={css.container}>
       <Navigation />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<HomePage />} />{" "}
-        {/* Перенаправлення на Home для невідомих маршрутів */}
-      </Routes>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="*" element={<HomePage />} />{" "}
+          {/* Перенаправлення на Home для невідомих маршрутів */}
+        </Routes>
+      </Suspense>
     </div>
   );
 }
